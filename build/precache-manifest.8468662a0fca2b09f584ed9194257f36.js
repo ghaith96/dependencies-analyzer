b50fa32c495ffdc7a46d234741a0b1c571179d93
ghaith96 <ghaithjardane@gmail.com>
2019-08-30 22:59:44 +0300

    GitHub pages release
    Signed-off-by: ghaith96 <ghaithjardane@gmail.com>

---
+++ build/precache-manifest.8468662a0fca2b09f584ed9194257f36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8219d31bff96f439b6c114d6749e5d01",
+    "url": "/dependencies-analyzer/index.html"
+  },
+  {
+    "revision": "c6f4499c29900b88d0df",
+    "url": "/dependencies-analyzer/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "8b4f5d5f6fb597ac494c",
+    "url": "/dependencies-analyzer/static/js/2.e524b19e.chunk.js"
+  },
+  {
+    "revision": "c6f4499c29900b88d0df",
+    "url": "/dependencies-analyzer/static/js/main.d01354b9.chunk.js"
+  },
+  {
+    "revision": "62e8e14ddf7c4f8e20bb",
+    "url": "/dependencies-analyzer/static/js/runtime~main.ef0170b7.js"
+  }
+]);
